46edd7a423cf7ef9f86e0c8749a2779df33cbfe5
Adeola <deolasalako@gmail.com>
2017-02-11 01:21:34 -0700

    updated fields in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  root to:"static_pages#{index}"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
